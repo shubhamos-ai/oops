6cb965ddd4c9a0981423bd9aa87e0a97b75847fc
shubhamos-ai <shubhamos.devilkings@gmail.com>
2026-09-18 16:38:42 +0530

    Sync: commands/base.py

---
+++ commands/base.py
@@ -0,0 +1,29 @@
+from bot import ModerationBot
+
+
+class Command:
+    def __init__(self, _: ModerationBot) -> None:
+        self.cmd = None
+
+    def register_self(self) -> None:
+        from command_registry import registry
+        if isinstance(self.cmd, list):
+            for cmd in self.cmd:
+                registry.register(cmd, self.__class__)
+        elif isinstance(self.cmd, str):
+            registry.register(self.cmd, self.__class__)
+        else:
+            raise ValueError(f"self.cmd must be of type list[str] or str!. Found type: {type(self.cmd)}")
+
+    def unregister_self(self) -> None:
+        from command_registry import registry
+        if isinstance(self.cmd, list):
+            for cmd in self.cmd:
+                registry.unregister(cmd)
+        elif isinstance(self.cmd, str):
+            registry.unregister(self.cmd)
+        else:
+            raise ValueError(f"self.cmd must be of type list[str] or str!. Found type: {type(self.cmd)}")
+
+    async def execute(self, *args, **kwargs) -> None:
+        pass
